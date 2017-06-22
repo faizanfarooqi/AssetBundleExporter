55c4d51c4af4b9848f9d0268ded446fb0b0f5561
faizanfarooqi <faizan.ur.rehman@tintash.com>
2017-06-22 14:24:48 +0500

    Update myscript.cs
    cleaned script with checks added

--- Editor/myscript.cs
+++ Editor/myscript.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.IO;
+using System.Text;
 using UnityEditor;
 
 public class myscript {
 
+	//we donot need these function as this is editor script being called without playmode
 	// Use this for initialization
 	/*void Start () {
 		Debug.Log("start method called");
@@ -18,78 +20,59 @@ public class myscript {
 	}
 	*/
 
-	
-	static public void check()
-	{
-		UnityEngine.Debug.Log("check");
-		FileStream F = new FileStream("C:\\Users\\Faizan\\Desktop\\test.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-         for (int i = 1; i <= 5; i++)
-         {
-            F.WriteByte((byte)i);
-         }
-         //BuildPipeline.BuildAssetBundles ("AssetBundles");
-	}
+	/*
+	Function to build asset bundles and export them to the specified directory
+	Created by Faizan-ur-Rehman 
+	Last updated 6/22/2017
+	*/
 	static void BuildAllAssetBundles ()
-    {
-    	UnityEngine.Debug.Log("check");
-		FileStream F = new FileStream("C:\\Users\\Faizan\\Desktop\\test.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-         for (int i = 1; i <= 5; i++)
-         {
-            F.WriteByte((byte)i);
-         }
-
-
-    	Debug.Log("In asset build function");
- 		//BuildPipeline.BuildAssetBundles("AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);  
- 		//UnityEngine.Debug.Log(AssetDatabase.FindAssets(""));
-
-
-    	/*
-    	Debug.Log("Main script is started");
-		UnityEngine.Object[] resources;
-		resources = Resources.LoadAll("");
-
-        foreach (var r in resources)
-        {
-            Debug.Log(r.name);
-            Debug.Log(AssetDatabase.GetAssetPath(r));
-            string assetPath = AssetDatabase.GetAssetPath(r);
-         	AssetImporter.GetAtPath(assetPath).SetAssetBundleNameAndVariant("testBundle", "");
-         	
-        }
-        BuildPipeline.BuildAssetBundles("newAssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
-		*/
-
-		//setting unity asset bundle names to none to export only assets set through resources
-        
-        /*var preResources = Resources.LoadAll("");
-
-        foreach (var r in preResources)
-        {
-        	Debug.Log("asset in all resources is " + r);
-
-            string preAssetPath = AssetDatabase.GetAssetPath(r);
-         	AssetImporter.GetAtPath(preAssetPath).SetAssetBundleNameAndVariant("", "");
-         }
-         */
-
+    {    	
 		Debug.Log("Main script is started");
+		string logText="In Editor script to export bundles";
 		UnityEngine.Object[] resources;
+		if(System.IO.Directory.Exists("C:\\Users\\Faizan\\Documents\\test\\Assets\\parrentResources")==false)
+		{
+			logText=logText+"\nPredefined path for resources in unity project (C:\\Users\\Faizan\\Documents\\test\\Assets\\parrentResources) does not exist";
+			return;
+		}
 		string[] intermediateAddress=System.IO.Directory.GetDirectories("C:\\Users\\Faizan\\Documents\\test\\Assets\\parrentResources");
+		if(intermediateAddress.Length!=1)
+		{
+			if(intermediateAddress.Length<1)
+			{
+				logText=logText+"\nFolder in parrentResources which must contain resources folder is not found.";
+			}
+			if(intermediateAddress.Length>1)
+			{
+				logText=logText+"\n";
+			}
+			string path = "C:\\Users\\Faizan\\Desktop\\assetBundleExporterLog.txt";
+			File.WriteAllText(path, logText);	
+			return;	
+		}
 		string uniqueFolderName=getFoldernameFromPath(intermediateAddress[0]);
+		if(System.IO.Directory.Exists("C:\\Users\\Faizan\\Documents\\test\\Assets\\parrentResources\\"+uniqueFolderName+"\\Resources")==false)
+		{
+			logText=logText+"\nResources folder does not exist";
+			string path = "C:\\Users\\Faizan\\Desktop\\assetBundleExporterLog.txt";
+			File.WriteAllText(path, logText);
+			return;
+		}
 		string[] resourceFolderNames=System.IO.Directory.GetDirectories("C:\\Users\\Faizan\\Documents\\test\\Assets\\parrentResources\\"+uniqueFolderName+"\\Resources");
+		
+		//Path exists. No assign asset bundles to all files corresponding to folder names
 		foreach (var folderPath in  resourceFolderNames)
 		{
 			var folder=getFoldernameFromPath(folderPath);
 			Debug.Log("folder name is "+folder);
-
-			//resources = Resources.LoadAll(getFoldernameFromPath(folder));
+			logText=logText+"\nfolder name is "+folder;
 			resources = Resources.LoadAll(folder);
 
 	        foreach (var r in resources)
 	        {
 
 	            Debug.Log("resource in folder "+folder+" is "+r.name);
+	            logText=logText+"\nresource in folder "+folder+" is "+r.name;
 	            Debug.Log(AssetDatabase.GetAssetPath(r));
 	            string assetPath = AssetDatabase.GetAssetPath(r);
 	         	AssetImporter.GetAtPath(assetPath).SetAssetBundleNameAndVariant(folder, "");
@@ -97,29 +80,21 @@ public class myscript {
 	        }
 		}
 
-
-		
-        
+        //Asset bundle names assigned. Now export bundles
         BuildPipeline.BuildAssetBundles("newAssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
 
-
- 		/*Process cmd = new Process();
-		cmd.StartInfo.FileName = "cmd.exe";
-		cmd.StartInfo.RedirectStandardInput = true;
-		cmd.StartInfo.RedirectStandardOutput = true;
-		cmd.StartInfo.CreateNoWindow = true;
-		cmd.StartInfo.UseShellExecute = false;
-		cmd.Start();
-
-		cmd.StandardInput.WriteLine("echo \"hello\" ");
-		cmd.StandardInput.Flush();
-		cmd.StandardInput.Close();
-		cmd.WaitForExit();
-		Console.WriteLine(cmd.StandardOutput.ReadToEnd()); 
-		*/ 
- 	}
+        //writing Log in manual file
+        string path = "C:\\Users\\Faizan\\Desktop\\assetBundleExporterLog.txt";
+		File.WriteAllText(path, logText);
 
 
+	}
+	
+	/*
+	Function that takes full absolute address and returns final folder name in the path
+	Created by Faizan-ur-Rehman
+	Last Updated 6/22/2017
+	*/
 	public  static string getFoldernameFromPath(string a_address)
 	{
 		int lastHashIndex=0;
@@ -137,5 +112,6 @@ public class myscript {
 		}
 		return Foldername;
 	}
+
 
 }
